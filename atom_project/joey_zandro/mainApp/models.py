00511479fc8a43b64565e6d4a798ae48eacf99cd
Nik Deryushev <nec-d@mail.ru>
2018-03-01 15:56:01 +0300

    first models

--- atom_project/joey_zandro/mainApp/models.py
+++ atom_project/joey_zandro/mainApp/models.py
@@ -21,4 +21,24 @@ class Category(models.Model):
 class Product(models.Model):
 
     category = models.ForeignKey(Category, related_name='products', verbose_name='Категория')
-    name = models.CharField(max_length=200, db_index=True)
+    name = models.CharField(max_length=200, db_index=True, verbose_name="Название")
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='', blank=True, verbose_name='Изображение')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    stock = models.PositiveIntegerField(verbose_name='В наличии')
+    available = models.BooleanField(default=True, verbose_name='Доступен')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+
+        ordering = ['name']
+        index_together = [
+            ['id', 'slug']
+        ]
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+    def __str__(self):
+        return self.name
